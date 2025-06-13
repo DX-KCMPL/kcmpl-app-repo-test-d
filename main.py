ada4461309b845e8bfb4adbc077a996cfb3178c4
Dan Poblete <dpoblete@kpmg.com.au>
2025-06-13 14:23:47 +1000

    Another change

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ def delete_file(filename):
 if __name__ == "__main__":
     user_input = input("Enter the filename to delete: ")
     delete_file(user_input)
+
+def delete_file(filename):
+    # WARNING: This is vulnerable to command injection
+    os.system("rm -rf " + filename)
+
+if __name__ == "__main__":
+    user_input = input("Enter the filename to delete: ")
+    delete_file(user_input)
