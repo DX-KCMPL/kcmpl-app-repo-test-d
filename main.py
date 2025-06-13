36775471225f90e8f94f85d9b55aea4e4de6638f
Dan Poblete <dpoblete@kpmg.com.au>
2025-06-13 14:43:04 +1000

    Attempt to trigger codeql failure

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 import os
 
-def delete_file(filename):
-    # WARNING: This is vulnerable to command injection
-    os.system("rm -rf " + filename)
-
-if __name__ == "__main__":
-    user_input = input("Enter the filename to delete: ")
-    delete_file(user_input)
-
-def delete_fil
+def vulnerable(user_input):
+    os.system("ls " + user_input)  # 🚨 CodeQL will detect this as a command injection
