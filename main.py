410037b2fb0fe8a56435ff926ddd6f9ae4e1810d
Dan Poblete <dpoblete@kpmg.com.au>
2025-06-13 14:28:21 +1000

    Trying to force a codeql error

--- main.py
+++ main.py
@@ -8,10 +8,4 @@ if __name__ == "__main__":
     user_input = input("Enter the filename to delete: ")
     delete_file(user_input)
 
-def delete_file(filename):
-    # WARNING: This is vulnerable to command injection
-    os.system("rm -rf " + filename)
-
-if __name__ == "__main__":
-    user_input = input("Enter the filename to delete: ")
-    delete_file(user_input)
+def delete_fil
