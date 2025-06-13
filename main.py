bb0ab8de139f952386e9eb1c0f1ff01eeecf9f78
Dan Poblete <dpoblete@kpmg.com.au>
2025-06-13 13:46:11 +1000

    Inserting code with vulnerability for testing

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+
+def delete_file(filename):
+    # WARNING: This is vulnerable to command injection
+    os.system("rm -rf " + filename)
+
+if __name__ == "__main__":
+    user_input = input("Enter the filename to delete: ")
+    delete_file(user_input)
